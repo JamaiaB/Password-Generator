688386cafddb4a9c3bc53a6797d7d4c4772cf406
JamaiaB <114833468+JamaiaB@users.noreply.github.com>
2022-10-31 21:01:08 -0500

    final changes

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specChar = ["@", "#", "$", "%", "^", "&", "*", "+", "?", "/", "~"];
 var input = [];
 var password = '';
-var guaranteedChar 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -20,15 +19,15 @@ function writePassword() {
 
 }
 
-
 function generatePassword() {
     var length = window.prompt("How many characters would you like to use?")
 
+    // Password length 8-128 characters. Can include numbers, letters and special characters.
     if (length < 8 || length > 128) {
         window.alert("Password must be between 8 and 128 characters!");
         return;
-    } 
-
+    }
+    // Create Prompts for password criteria
     var userLowCase = window.confirm("Would you like to include lower case characters?");
     var userUpperCase = window.confirm("Would you like to include upper case characters?");
     var userNum = window.confirm("Would you like to include numbers?");
@@ -37,36 +36,39 @@ function generatePassword() {
     if (userUpperCase) {
         input += upperCase;
     }
-    
+
     if (userLowCase) {
         input += lowCase;
     }
-    
+
     if (userNum) {
         input += num;
     }
-    
+
     if (userSpecChar) {
         input += specChar;
     }
-    
+    // Confirm the use of uppercase, lowercase, numeric and/or special characters.
     if (!userUpperCase && !userLowCase && !userNum && !userSpecChar) {
         window.alert("Invalid! You must make at least one selection!")
         writePassword()
     }
     for (var i = 0; i < length; i++) {
         var randomNumber = Math.floor(Math.random() * input.length);
-        console.log("password;" , password)
-        console.log("input;" , input)
+        console.log("password;", password)
+        console.log("input;", input)
         password += input[randomNumber];
     }
-    console.log(password)
+    return password
+
+    
+    // When prompts are answered correctly, I can generate a password. 
+    //return generatePassword()
 }
 
-// Create Prompts for password criteria
-// Password length 8-128 characters. Can include numbers, letters and special characters.
-// Confirm the use of uppercase, lowercase, numeric and/or special characters.
-// When prompts are answered correctly, I can generate a password. 
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
